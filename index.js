e6904d479d67c91c7b9188e7751ab240ac7278a0
KNBrgb <kabriab227@gmail.com>
2024-08-31 18:18:45 -0400

    cat foreach

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ function fetchCategories() {
         .then(data => {
             const categoriesContainer = document.getElementById('categories-container');
             const categories = data.categories;
-        });
+
+            categories.forEach(category => {
+                const categoryDiv = document.createElement('div') ;
+                categoryDiv.classList.add('category');
+
+                const categoryImage = document.createElement('img');
+                categoryImage.src = category.strCategoryThumb;
+                categoryImage.alt = category.strCategory;
+                categoryDiv.appendChild(categoryImage);
+
+                const categoryName = document.createElement('h3');
+                categoryName.textContent = category.strCategory;
+                categoryDiv.appendChild(categoryName);
+
+                categoriesContainer.appendChild(categoryDiv);
+            });
+        })
         
 }
 window.onload = fetchCategories;
