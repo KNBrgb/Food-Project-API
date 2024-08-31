355bcca6534ae0ad4823628ef774ebfb7680c022
KNBrgb <kabriab227@gmail.com>
2024-08-31 18:01:14 -0400

    then for data created

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function fetchCategories() {
-    fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+        .then(response => response.json())
+        .then(data => {
+            const categoriesContainer = document.getElementById('categories-container');
+            const categories = data.categories;
+        });
+        
 }
+window.onload = fetchCategories;
